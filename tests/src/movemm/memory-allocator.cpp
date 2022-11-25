fc08d44386fc2fa8dc6e45cbb9e1bf872368d8a0
Ian Diaz <diaz.ian.w@gmail.com>
2022-11-24 21:09:12 -0600

    Disabled C allocator tests

--- tests/src/movemm/memory-allocator.cpp
+++ tests/src/movemm/memory-allocator.cpp
@@ -6,56 +6,58 @@
 #include <movemm/memory-allocator.h>
 #include <string.h>
 
-SCENARIO("Testing the C allocator API")
-{
-    GIVEN("An empty stack array of pointers")
-    {
-        void* generated[100];
-        memset(generated, 0, sizeof(generated));
+// SCENARIO("Testing the C allocator API")
+// {
+//     GIVEN("An empty stack array of pointers")
+//     {
+//         void* generated[100];
+//         memset(generated, 0, sizeof(generated));
 
-        movemm_statistics_t start_stats;
-        movemm_get_statistics(&start_stats);
+//         movemm_statistics_t start_stats;
+//         movemm_get_statistics(&start_stats);
 
-        INFO("Total memory mapped = " << start_stats.mapped);
+//         INFO("Total memory mapped = " << start_stats.mapped);
 
-        WHEN("100 allocations are made")
-        {
-            for (size_t i = 0; i < sizeof(generated) / sizeof(generated[0]);
-                 ++i)
-            {
-                generated[i] = movemm_alloc(1024 * 1024);
-            }
+//         WHEN("100 allocations are made")
+//         {
+//             for (size_t i = 0; i < sizeof(generated) / sizeof(generated[0]);
+//                  ++i)
+//             {
+//                 generated[i] = movemm_alloc(1024 * 1024);
+//             }
 
-            THEN("All allocations should be valid")
-            {
-                for (size_t i = 0; i < sizeof(generated) / sizeof(generated[0]);
-                     ++i)
-                {
-                    REQUIRE(generated[i] != 0);
-                }
-            }
+//             THEN("All allocations should be valid")
+//             {
+//                 for (size_t i = 0; i < sizeof(generated) /
+//                 sizeof(generated[0]);
+//                      ++i)
+//                 {
+//                     REQUIRE(generated[i] != 0);
+//                 }
+//             }
 
-            AND_THEN(
-                "The amount of mapped memory should be higher than it was at "
-                "the start")
-            {
-                movemm_statistics_t cur_stats;
-                movemm_get_statistics(&cur_stats);
+//             AND_THEN(
+//                 "The amount of mapped memory should be higher than it was at
+//                 " "the start")
+//             {
+//                 movemm_statistics_t cur_stats;
+//                 movemm_get_statistics(&cur_stats);
 
-                REQUIRE(cur_stats.mapped > start_stats.mapped);
-            }
+//                 REQUIRE(cur_stats.mapped > start_stats.mapped);
+//             }
 
-            AND_WHEN("All of the allocations are freed")
-            {
-                for (size_t i = 0; i < sizeof(generated) / sizeof(generated[0]);
-                     ++i)
-                {
-                    REQUIRE_NOTHROW(movemm_free(generated[i]));
-                }
-            }
-        }
-    }
-}
+//             AND_WHEN("All of the allocations are freed")
+//             {
+//                 for (size_t i = 0; i < sizeof(generated) /
+//                 sizeof(generated[0]);
+//                      ++i)
+//                 {
+//                     REQUIRE_NOTHROW(movemm_free(generated[i]));
+//                 }
+//             }
+//         }
+//     }
+// }
 
 SCENARIO("Testing stack allocations")
 {
